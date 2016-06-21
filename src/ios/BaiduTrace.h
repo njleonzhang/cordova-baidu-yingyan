704142bb03baf8ed978d54f197e2071737919519
njleonzhang <zpczjl@126.com>
2016-06-21 11:42:57 +0800

    check in header file

--- src/ios/BaiduTrace.h
+++ src/ios/BaiduTrace.h
@@ -1,9 +1,18 @@
 #import <Cordova/CDVPlugin.h>
 #import <BaiduTraceSDK/BaiduTraceSDK-Swift.h>
 
-@interface BaiduTrace : CDVPlugin <ApplicationServiceDelegate, ApplicationEntityDelegate, ApplicationTrackDelegate>
+@interface BaiduTrace : CDVPlugin <ApplicationServiceDelegate, ApplicationEntityDelegate, ApplicationTrackDelegate>{
+    @protected
+    NSString* _AK;
+    NSString* _MCode;
+    BTRACE * _traceInstance;
+    NSMutableDictionary* _customAttr;
+}
 
 - (void)startTrace:(CDVInvokedUrlCommand*)command;
 - (void)stopTrace:(CDVInvokedUrlCommand*)command;
+- (void)setInterval:(CDVInvokedUrlCommand*)command;
+- (void)setLocationMode:(CDVInvokedUrlCommand*)command;
+- (void)setProtocolType:(CDVInvokedUrlCommand*)command;
 
 @end
